c04f02df3815a45dfed75b33145ec575e045b259
simon waronja <simon.waronja@student.moringaschool.com>
2022-06-28 12:24:08 +0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const number = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === number) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n2) -nested loop
 */
 
 /* 
   Add your pseudocode here
+
+  loop through the array with a for loop for our first index
+
+  declares a variable called number that is equal to target meaning array[i]
+
+  loops through the array again for our second index
+
+  if the array[second index] is equal to our number
+  return true
+    else
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  
+  returns true if any set of two numbers sums up to equal the target
 */
 
 // You can run `node index.js` to view these console logs
